b97ced8e993650a49ece9c10236991fb3223006d
lisperow <lisperow@yandex.ru>
2019-11-07 20:35:21 +0300

    add reagent

--- src/bluesland/time_dive.cljs
+++ src/bluesland/time_dive.cljs
@@ -1,3 +1,14 @@
-(ns bluesland.time-dive)
+(ns ^:figwheel-hooks bluesland.time-dive
+  (:require [reagent.core :as r]))
 
-(js/console.log "Hello, Time Dive!")
+(defn app []
+  [:h1.site__title
+   [:span.site__title-text "Time Dive"]])
+
+(defn mount []
+  (r/render [app] (js/document.getElementById "root")))
+
+(defn ^:after-load re-render []
+  (mount))
+
+(defounce start-up (do (mount) true))
